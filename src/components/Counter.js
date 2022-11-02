97e15d1f0653f70aaae047b7531082edd03ff32b
Tom Rossner <tomrossner2@gmail.com>
2022-11-02 10:44:52 +0200

    Cleanup

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -15,10 +15,10 @@ const Counter = ({configurations}) => {
     const setColor = (min, max) => {
         min = Number(min);
         max = Number(max);
-        if(min <= 0 && max < 20){return "lightcoral"}
-        if(min <= 10 && max < 50){return "lightgreen"}
-        if(min <= 15 && max < 100){return "lightyellow"}
-        if(isNaN(min) || isNaN(max)){return "black"}
+        if(min <= 0 && max < 20) return "lightcoral";
+        if(min <= 10 && max < 50) return "lightgreen";
+        if(min <= 15 && max < 100) return "lightyellow";
+        if(isNaN(min) || isNaN(max)) return "black";
         else return "pink";
       }
 
